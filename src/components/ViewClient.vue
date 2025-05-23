1dcdae8c4dd63103eaaf481f8a5eda301facb7e2
seth1nk <kostik.vs10@mail.ru>
2025-05-23 19:53:26 +0800

    Update ViewClient.vue

--- src/components/ViewClient.vue
+++ src/components/ViewClient.vue
@@ -37,7 +37,7 @@ export default {
       client: null,
       loading: true,
       errorMessage: null,
-      backendUrl: 'http://localhost:3000',
+      backendUrl: 'https://uvue.onrender.com',
     };
   },
   async created() {
