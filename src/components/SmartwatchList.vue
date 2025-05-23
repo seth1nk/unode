ebd2342c5c2c6d134f98bf7c53f570d362d0129d
seth1nk <kostik.vs10@mail.ru>
2025-05-23 19:53:15 +0800

    Update SmartwatchList.vue

--- src/components/SmartwatchList.vue
+++ src/components/SmartwatchList.vue
@@ -64,7 +64,7 @@ export default {
       currentPage: 1,
       totalPages: 0,
       pageSize: 10,
-      backendUrl: 'http://localhost:3000',
+      backendUrl: 'https://uvue.onrender.com',
     };
   },
   async created() {
